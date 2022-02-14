b1caa15c2fde18906c02d425f780fbf96938e383
Ricard Gascons <gascons1995@gmail.com>
2022-02-13 18:16:25 -0800

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,64 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Index.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Ricard Gascons</title>
+        <meta name="description" content="Personal portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <header className={styles.header}>
+          <h1 className={styles.title}>
+            Ricard Gascons
+          </h1>
+          <nav className={styles.icons}>
+            <a href='mailto:gascons1995@gmail.com'>email</a>
+            <a href=''>cv</a>
+            <a href='https://www.linkedin.com/in/ricard-gascons/'>ld</a>
+            <a href='https://github.com/rgascons'>gh</a>
+          </nav>
+        </header>
+        
+
+        <section>
+          <h2 className={styles.subtitle}>About me</h2>
+          <ul className={styles.list}>
+            <li>Based out of Seattle, Washington, USA</li>
+            <li>😺 and 🐶 lover</li>
+          </ul>
+        </section>
+        <section>
+          <h2 className={styles.subtitle}>What I do</h2>
+          <ul className={styles.list}>
+            <li>React Experiences</li>
+            <li>Frontend arquitecture</li>
+            <li>React, Redux and Typescript</li>
+          </ul>
+        </section>
+        <section>
+          <h2 className={styles.subtitle}>Side projects</h2>
+          <div className={styles.grid}>
+            <a href="https://github.com/rgascons/MiniBlockchain"
+               className={styles.card}>
+              <h2>MiniBlockchain &rarr;</h2>
+              <p>Blockchain written in .NET Core that can mine/send/receive currency.</p>
+            </a>
+
+            <a href="https://github.com/jutge-org/cmm"
+               className={styles.card}>
+              <h2>Cmm &rarr;</h2>
+              <p>Cmm is a C++ interpreter that can be embedded in the browser.</p>
+            </a>
+          </div>
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default Home
